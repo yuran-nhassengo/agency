b69065754ebd41f40b0924ab91fef2c15d32491c
yurannhassengo <yurannhassengo@gmail.com>
2024-07-29 13:32:30 -0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'neutralSilver': '#F5F7FA',
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#4CAF4F',
+        'neutralGrey':'#717171',
+      }
+    },
+  },
+  plugins: [],
+}
+
