0f7f52fdcc5c1830a1b881d94b81eba68c734b04
yurannhassengo <yurannhassengo@gmail.com>
2024-07-30 12:16:48 -0700

    add responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ export const NavBar = () => {
 
   return (
     <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-        <nav className={`py-4 lg:px-14  px-4 ${isSticky} "sticky top-0 left-0 right-0 border-b  bg-white duration-300`}>
+        <nav className={`py-4 lg:px-14  px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b" :" bg-white duration-300"}`}>
             <div className=' flex justify-between items-center text-base gap-8'>
                 <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={logo} alt="logo" className='w-10 inline-block items-center' /><span className='text-[#263238]'>Agencia</span></a>
 
@@ -55,7 +55,7 @@ export const NavBar = () => {
                 </ul>
 
                 <div className='space-x-12 hidden lg:flex items-center'>
-                    <a href="" className='hidden lg:flex items-center text-brandPrimary
+                    <a href="/" className='hidden lg:flex items-center text-brandPrimary
                     hover:text-gray900'>Login</a>
                     <button className='bg-brandPrimary text-white py-2 px-4 transition-all
                     duration-300 rounded hover:bg-neutralDGrey'>Sign Up</button>
@@ -73,6 +73,18 @@ export const NavBar = () => {
                     </button>
 
                 </div>
+            </div>
+
+            <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${isMenuOpen ?"block fixed top-0 right-0 left-0" :"hidden"} `}>
+
+                    {
+                        navItems.map(({link,path}) =><Link to={path} spy={true} smooth={true} 
+                        offset={-100} key={path} className='block text-base text-gray900 hover:text-brandPrimary
+                        first:font-medium'>{link}</Link>)
+                    }
+
+
+
             </div>
         </nav>
     </header>
